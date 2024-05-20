ac00db6ee4449eb8b579ad2225e1c2b05ef6ec4e
p3xsouger <151133481+Sulaimannabdul@users.noreply.github.com>
2024-05-20 08:58:41 +0700

    index.ts

---
+++ frida/agent/index.ts
@@ -0,0 +1,37 @@
+import { log } from "./logger";
+import * as Runtime from "./runtime";
+import {SDParser} from "./SDParser";
+
+
+
+log("--- loaded ---");
+
+Runtime.initHook();
+
+function getMainModule():Module|null {
+    let exePath = ObjC.classes.NSBundle.mainBundle().executablePath() as string;
+    let modules = Process.enumerateModules();
+    for (var i = 0; i < modules.length; i++) {
+        let oneModule = modules[i];
+        if (oneModule.path == exePath) {
+            return oneModule;
+        }
+    }
+    return null;
+}
+
+let mainModule = getMainModule();
+if (mainModule) {
+    log('main module path ' + mainModule.path)
+    log('main module base ' + mainModule.base)
+    if (Runtime.isSwiftAvailable()) {
+        let parser = new SDParser(mainModule);
+        parser.parseSwiftType();
+        parser.dumpAll();
+    } else {
+        log('swift is NOT available');
+    }
+    
+} else {
+    log('fail to find main module');
+}
