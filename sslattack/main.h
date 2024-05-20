8fa2c949f8bd20898ed37a5208086684abbf7eb2
p3xsouger <151133481+Sulaimannabdul@users.noreply.github.com>
2024-05-20 08:43:12 +0700

    main.h

---
+++ sslattack/main.h
@@ -0,0 +1,44 @@
+#ifndef main_h
+#define main_h
+
+#include <stddef.h>
+#include <stdint.h>
+
+#if !defined(FISHHOOK_EXPORT)
+#define MAIN_VISIBILITY __attribute__((visibility("hidden")))
+#else
+#define MAIN_VISIBILITY __attribute__((visibility("default")))
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif //__cplusplus
+    
+    /*
+     * A structure representing a particular intended rebinding from a symbol
+     * name to its replacement
+     */
+    struct rebinding {
+        const char *name;
+        void *replacement;
+        void **replaced;
+    };
+    
+    MAIN_VISIBILITY
+    int rebind_symbols(struct rebinding rebindings[], size_t rebindings_nel);
+    
+    /*
+     * Rebinds as above, but only in the specified image. The header should point
+     * to the mach-o header, the slide should be the slide offset. Others as above.
+     */
+    MAIN_VISIBILITY
+    int rebind_symbols_image(void *header,
+                             intptr_t slide,
+                             struct rebinding rebindings[],
+                             size_t rebindings_nel);
+    
+#ifdef __cplusplus
+}
+#endif //__cplusplus
+
+#endif //main_h
